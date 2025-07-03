589cc85fbc7bf066a5d1dd54d42abc63a11fc1d0
Anton Reshetov <reshetov.art@gmail.com>
2025-07-03 09:50:39 +0300

    feat!: migrate to massCode v4 support (#3)
    BREAKING CHANGE:
    - Dropped support for massCode v3 due to API and architecture changes.
    - Updated dependencies and configurations to align with massCode v4 requirements.

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,40 @@
-import { Action, ActionPanel, Icon, List, showToast, Toast } from "@raycast/api";
+import { Action, ActionPanel, Icon, List, showToast, Toast, getPreferenceValues } from "@raycast/api";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import type { Snippet, State } from "./types";
+import type { Snippet, State, ListItem } from "./types";
+import { MESSAGES } from "./contants";
+
+interface Preferences {
+  port: string;
+}
+
+const { port } = getPreferenceValues<Preferences>();
+const PORT = parseInt(port, 10) || 4321;
 
 export default function Command() {
-  const [state, setState] = useState<State>({ snippets: [] });
+  const [state, setState] = useState<State>({ list: [] });
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get<Snippet[]>("http://localhost:3033/snippets/embed-folder");
-        setState({ snippets: res.data });
+        const { data} = await axios.get<Snippet[]>(`http://localhost:${PORT}/snippets`);
+        const options = data.reduce((acc: ListItem[], snippet) => {
+          snippet.contents.forEach((content) => {
+            acc.push({
+              id: content.id,
+              name: content.label,
+              snippetName: snippet.name,
+              detail: `${content.label} • ${content.language}`,
+              description: `${snippet.folder?.name || 'Inbox'}`,
+              value: content.value ?? '',
+              language: content.language,
+            })
+          })
+          return acc
+        }, [])
+        setState({ list: options });
       } catch (err) {
-        setState({ error: err instanceof Error ? err : new Error("Something went wrong") });
+        setState({ error: err instanceof Error ? err : new Error(MESSAGES.ERROR) });
       }
     };
 
@@ -20,22 +42,29 @@ export default function Command() {
   }, []);
 
   if (state.error) {
-    showToast(Toast.Style.Failure, "massCode app is not running");
+    showToast(Toast.Style.Failure, MESSAGES.ERROR);
   }
 
   return (
-    <List searchBarPlaceholder="Type to search snippets">
-      {state.snippets?.map((i) => {
+    <List isShowingDetail searchBarPlaceholder="Type to search snippets">
+      {state.list?.map((i) => {
+        const markdownDetail = `**Fragment:** ${i.name}` +
+        `\n\n**Language:** ${i.language}\n` +
+        '```' + i.language + '\n' + i.value + '\n```'
         return (
           <List.Item
             key={i.id}
-            title={i.name}
-            subtitle={i.content[0].language}
+            title={i.snippetName}
             icon={Icon.Document}
-            accessories={[{ text: i.folder?.name ?? 'Inbox' }]}
+            accessories={[{ text: i.description }]}
+            detail={
+              <List.Item.Detail
+                markdown={markdownDetail}
+              />
+            }
             actions={
               <ActionPanel title="Some">
-                <ActionPanel.Section>{<Action.CopyToClipboard content={i.content[0].value} />}</ActionPanel.Section>
+                <ActionPanel.Section>{<Action.CopyToClipboard content={i.value} />}</ActionPanel.Section>
               </ActionPanel>
             }
           ></List.Item>
